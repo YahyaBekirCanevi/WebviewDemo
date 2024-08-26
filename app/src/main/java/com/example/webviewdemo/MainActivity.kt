acbabf3bf26789ad3dc23a9c094715dfb45b8f7a
YahyaBekirCanevi <93769780+YahyaBekirCanevi@users.noreply.github.com>
2024-08-26 23:52:49 +0300

    jetpack compose

---
+++ app/src/main/java/com/example/webviewdemo/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.webviewdemo
+
+import android.os.Bundle
+import android.view.ViewGroup
+import android.webkit.ConsoleMessage
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+import com.example.webviewdemo.ui.theme.WebviewDemoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            WebViewPage(
+                url = "https://www.iosroadmap.com",
+            )
+        }
+    }
+}
+
+@Composable
+fun WebViewPage(url: String) {
+    AndroidView(factory = {
+        WebView(it).apply {
+            layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+            )
+
+            webViewClient = CustomWebViewClient()
+        }
+    }, update = {
+        it.loadUrl(url)
+    })
+}
+
+class CustomWebViewClient: WebViewClient()
